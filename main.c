29fc879ee658b80b42869c1bf64536687d802027
Javier de la Cruz Sánchez <javier023javi@gmail.com>
2024-12-24 16:31:53 +0100

    feat: adding functions and definitions

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define A 80
+
+typedef struct {
+    unsigned dia, mes, anyo;
+} DATE;
+
+struct ALUMNO {
+    char DNI[9+1];
+    char nom_comp[20+1];
+    DATE nac;
+};
+
+void all_swap(struct ALUMNO *, struct ALUMNO *);
+void all_bubbleSort(struct ALUMNO  [A], unsigned);
+void fprint_DATE(FILE *, DATE);
+void fprint_ALUMNO (FILE *, struct ALUMNO);
+
 
 int main(void)
 {
-    printf("Hello, World!\n");
     return 0;
 }
